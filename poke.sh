c39e0064fe2257cc7c114dadb027b74f0e214397
Captain Wilder <42346843+CaptainWilder@users.noreply.github.com>
2023-10-04 16:19:17 -0500

    Update poke.sh
    Removed commented-out code. 
    Removed old IP references to TTI
    Cleaned up output

--- poke.sh
+++ poke.sh
@@ -2,21 +2,19 @@
 # This script performs a DNS lookup and returns various records for a given domain.
 # Make sure a domain is provided as an argument
 # https://github.com/CaptainWilder/
+
+#Get Input/Argument
 if [ -z "$1" ]; then
-          echo "Usage: dns_lookup.sh DOMAIN"
+          echo "Usage: poke.sh DOMAIN"
             exit 1
 fi
 
 # Perform an A record lookup and get the IP
 ip=$(dig +short A "$1")
+ip_org=$(echo "$ip" | head -n1 )
 
-# Check if the IP is in the 38.97.16.0/24 or 38.67.13.0/24 networks
-if [[ "$ip" =~ ^38\.97\.16\.[0-9]{1,3}$ || "$ip" =~ ^38\.67\.13\.[0-9]{1,3}$ ]]; then
-          org="TRINSIC"
-  else
-            # Perform a whois lookup on the IP and extract the Organization field
-              org=$(whois "$ip" | grep 'Organization:' | awk '{print $2}')
-fi
+# Perform a whois lookup on the IP and extract the Organization field
+org=$(whois "$ip_org" | grep 'Organization:' | awk '{print $2}')
 
 # Perform an MX record lookup
 mx=$(dig +short MX "$1")
@@ -31,25 +29,13 @@ cname=$(dig +short CNAME "www.$1")
 txt=$(dig +short TXT "$1" | grep spf)
 
 # Perform a WHOIS search and extract the registrar name
-registrar=$(whois "$1" | grep -m 1 'Registrar:')
+registrar=$(whois "$1" | grep -m 1 'Registrar:' | awk '{$1=$1;print}')
 
 # Output the results
-echo "A record: $ip"
+echo "IP: $ip"
 echo "WebHost: $org"
-echo "WHOIS $registrar"
-echo " CNAME record: $cname"
+echo "$registrar"
+echo "CNAME record: $cname"
 echo "MX record: $mx"
 echo "NS records: $ns"
 echo "SPF: $txt"
-
-
-#ports=( 21 22 23 25 53 80 110 123 143 161 389 443 445 465 993 587 995 3306 )
-# Iterate through the array of port numbers
-#for port in "${ports[@]}"
-#do
-          # Use nc to check if the port is open on the target host
-#           if nc -z "$1" "$port"
-#                     then
-#                                 echo "Port $port is open on $1"
-#                                   fi
-#                           done
