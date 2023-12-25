1000f738f626ac1c3216fb3ae2d0aaca7b0bd18f
Captain Wilder <42346843+CaptainWilder@users.noreply.github.com>
2023-12-25 07:57:20 -0600

    Update poke.sh

--- poke.sh
+++ poke.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# This script performs a DNS lookup and returns various records for a given domain.
-# Make sure a domain is provided as an argument
-# https://github.com/CaptainWilder/
+#######################
+###Wilder Army Knife###
+######DNS Tools########
+#######################
+
 
 #Get Input/Argument
 if [ -z "$1" ]; then
@@ -9,29 +11,29 @@ if [ -z "$1" ]; then
             exit 1
 fi
 
-# Perform an A record lookup and get the IP
+#A record lookup and get the IP
 ip=$(dig +short A "$1")
 ip_org=$(echo "$ip" | head -n1 )
 
-# Perform a whois lookup on the IP and extract the Organization field
+#Whois lookup on the IP and extract the Organization field
 org=$(whois "$ip_org" | grep 'Organization:' | awk '{print $2}')
 
-# Perform an MX record lookup
+#MX record lookup
 mx=$(dig +short MX "$1")
 
-# Perform an NS record lookup
+#NS record lookup
 ns=$(dig +short NS "$1")
 
-# Perform a CNAME record lookup
+#CNAME record lookup
 cname=$(dig +short CNAME "www.$1")
 
-# Perform a TXT record lookup
+#TXT record lookup
 txt=$(dig +short TXT "$1" | grep spf)
 
-# Perform a WHOIS search and extract the registrar name
+#WHOIS search and extract the registrar name
 registrar=$(whois "$1" | grep -m 1 'Registrar:' | awk '{$1=$1;print}')
 
-# Output the results
+#Output
 echo "IP: $ip"
 echo "WebHost: $org"
 echo "$registrar"
